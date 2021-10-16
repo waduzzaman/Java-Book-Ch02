64bdcb799c1e17b58d6644c08c8000444ceb201d
Waduzzaman <waduzzaman@gmail.com>
2021-10-16 08:05:41 -0400

    chapter02 first commit

---
+++ src/exercise1/Main.java
@@ -0,0 +1,9 @@
+package exercise1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Welcome to Java Programming language.");
+    }
+}
