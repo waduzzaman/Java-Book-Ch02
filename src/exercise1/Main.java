fc4488cf96896b31df4f18ae3f094663e4c754e5
Waduzzaman <waduzzaman@gmail.com>
2021-10-16 11:00:21 -0400

    update

--- src/exercise1/Main.java
+++ src/exercise1/Main.java
@@ -1,9 +1,21 @@
+
 package exercise1;
+
+// My first Java Program. I am going to use Java Book's Chapter 02 examples.
+
+// Every java program must have at least one class.
+
+//public class are lower case. Main lower case.
+// Class name starts with Capital letter and then each word thereafter also Capital letter
+// like: ClassName
+// its a CamelCase Convension
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Write a string line to see the output
         System.out.println("Welcome to Java Programming language.");
+
+        // " " between the quotation is called String or String Character or String Literal
     }
 }
